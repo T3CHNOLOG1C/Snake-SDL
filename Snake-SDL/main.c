9b7f0478c15b3ec6e47ca74eb7587d8cc638eafe
Hill Ma <mahiuchun@users.noreply.github.com>
2013-11-16 22:14:31 +0800

    Initial Commit

---
+++ Snake-SDL/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Snake-SDL
+//
+//  Created by Ma Xiaojun on 11/16/13.
+//  Copyright (c) 2013 Example. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
